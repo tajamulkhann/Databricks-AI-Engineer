3175a596f82531fbb2ad7ec8ff14bbd3e6b3f343
Tajamul Khan <tajamulk@gmail.com>
2026-04-12 15:28:09 +0530

    add

---
+++ Code/Basic RAG Params.py
@@ -0,0 +1,8 @@
+# For this first basic demo, we'll keep the configuration as a minimum. In real app, you can make all your RAG as a param (such as your prompt template to easily test different prompts!)
+
+chain_config = {
+    "llm_model_serving_endpoint_name": "databricks-meta-llama-3-3-70b-instruct",  # the foundation model we want to use
+    "vector_search_endpoint_name": "demoeps",  # the endoint we want to use for vector search
+    "vector_search_index": "workspace.default.my_index",
+    "llm_prompt_template": """You are an assistant that answers questions. Use the following pieces of retrieved context to answer the question. Some pieces of context may be irrelevant, in which case you should not use them to form the answer.\n\nContext: {context}""",
+}
